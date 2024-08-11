d9884e72d9b29ccc4d827c273ef5eb8f3f14517c
Mahdi-sn10 <xboy.pleyer10@gamil.com>
2024-08-11 10:44:13 +0330

    add

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class posts(models.Model):
     title = models.CharField(max_length=20)
-    writer = models.CharField(max_length=30)
-    body = models.TextField()
+    auther = models.ForeignKey(User,on_delete=models.CASCADE)
     created_time = models.DateField(auto_now_add=True)
     edit_time = models.DateField(auto_now=True)
     image = models.ImageField(upload_to="image/posts")
