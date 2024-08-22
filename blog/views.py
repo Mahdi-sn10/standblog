24c62910069ff0dc735d97820e7f722f670d5f87
Mahdi-sn10 <xboy.pleyer10@gamil.com>
2024-08-22 16:17:58 +0330

    mmd

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import posts
 # Create your views here.
+def post_detale(request,pk):
+
+    post = posts.objects.get(id=pk)
+    return render(request,"blog/post-details.html",{"post":post})
--- standblog/urls.py
+++ standblog/urls.py
@@ -21,7 +21,8 @@ from . import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include("home.urls")),
-    path("account/",include("account.urls"))
+    path("account/",include("account.urls")),
+    path("post/",include("blog.urls"))
 
 ]
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
